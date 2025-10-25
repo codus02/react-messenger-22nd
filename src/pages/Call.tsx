09276376ca7e18c1de67bc757e1a29d7067b726b
codus02 <chaetree02@gmail.com>
2025-10-25 15:47:31 +0900

    call 스피너

--- src/pages/Call.tsx
+++ src/pages/Call.tsx
@@ -1,10 +1,13 @@
 // src/pages/Call.tsx
 import MenuTab from '@/layouts/MenuTab';
+import { SyncLoader } from 'react-spinners';
 
 export default function Call() {
   return (
     <MenuTab>
-      <div className="flex h-full bg-[var(--white)]">{/* 빈 화면 */}</div>
+      <div className="flex h-full flex-1 items-center justify-center">
+        <SyncLoader color="var(--gray-700)" size={15} margin={8} aria-label="Loading Spinner" />
+      </div>
     </MenuTab>
   );
 }
